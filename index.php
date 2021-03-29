4606021bded992ad608ba44677186bbd6e902f42
David <davidlecras@hotmail.com>
2021-03-29 14:52:59 +0200

    finalize file

--- index.php
+++ index.php
@@ -1,61 +1,142 @@
 <?php
-if (!empty($_POST['urlBitly'])) {
-    $url = $_POST['urlBitly'];
+// IS RECEIVED SHORTCUT
+if (isset($_GET['q'])) {
+
+    // VARIABLE
+    $shortcut = htmlspecialchars($_GET['q']);
+
+    // IS A SHORTCUT ?
+    $bdd = new PDO('mysql:host=localhost;dbname=bitly;charset=utf8', 'root', '');
+    $req = $bdd->prepare('SELECT COUNT(*) AS x FROM links WHERE shortcut = ?');
+    $req->execute(array($shortcut));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 1) {
+            header('location: ../?error=true&message=Adresse url non connue');
+            exit();
+        }
+    }
+
+    // REDIRECTION
+    $req = $bdd->prepare('SELECT * FROM links WHERE shortcut = ?');
+    $req->execute(array($shortcut));
+
+    while ($result = $req->fetch()) {
+
+        header('location: ' . $result['url']);
+        exit();
+    }
 }
 
-// Vérification ldu champs lors de l'envoi:
-// Verifier si hasBeenAlreadySend:
+// IS SENDING A FORM
+if (isset($_POST['url'])) {
 
-var_dump($url);
+    // VARIABLE
+    $url = $_POST['url'];
+
+    // VERIFICATION
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        // PAS UN LIEN
+        header('location: ../?error=true&message=Adresse url non valide');
+        exit();
+    }
+
+    // SHORTCUT
+    $shortcut = crypt($url, rand());
+
+    // HAS BEEN ALREADY SEND ?
+    $bdd = new PDO('mysql:host=localhost;dbname=my_bitly;charset=utf8', 'root', 'root');
+    $req = $bdd->prepare('SELECT COUNT(*) AS x FROM links WHERE url = ?');
+    $req->execute(array($url));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 0) {
+            header('location: ../?error=true&message=Adresse déjà raccourcie');
+            exit();
+        }
+    }
+
+    // SENDING
+    $req = $bdd->prepare('INSERT INTO links(url, shortcut) VALUES(?, ?)');
+    $req->execute(array($url, $shortcut));
+
+    header('location: ../?short=' . $shortcut);
+    exit();
+}
 ?>
-
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public//css/style.css">
-    <link rel="icon" type="image/png" href="./public/images/favico.png" />
-    <title>Mon bitly</title>
+    <meta charset="utf-8">
+    <title>Raccourcisseur d'url express</title>
+    <link rel="stylesheet" type="text/css" href="./public/css/style.css">
+    <link rel="icon" type="image/png" href="./public/images/favico.png">
 </head>
 
 <body>
-    <main role="main">
-        <!-- Section hello -->
-        <section id="hello">
-            <div class="container">
-                <header><img src="./public/images/logo.png" alt="logo bitly" id="logo"></header>
-                <h1>Une URL longue? Racourcicez-la</h1>
-                <h2>Largement meilleur et plus court que les autres.</h2>
-                <form action="index.php" method="post">
-                    <input type="url" name="urlBitly" id="myUrl" placeholder="URL à racourcir">
-                    <input type="submit" value="Racourcir">
-                </form>
-            </div>
-        </section>
-        <!-- section brandq -->
-        <section id="brands">
-            <!-- container -->
-            <div class="container">
-                <h3>Ces entreprises qui nous font confiance</h3>
-                <img src="./public//images/1.png" alt="brands" class="picture">
-                <img src="./public/images/2.png" alt="brands" class="picture">
-                <img src="./public/images/3.png" alt="brands" class="picture">
-                <img src="./public/images/4.png" alt="brands" class="picture">
-            </div>
+    <!-- PRESENTATION -->
+    <section id="hello">
 
-        </section>
-        <footer class="container">
-            <div id="footer">
-                <img src="./public/images/logo2.png" alt="logo bitly" id="logo2">
-                <p>Bilty ® 2021</p>
-                <a href="#">Contact</a> - <a href="#">À propos</a>
-            </div>
-        </footer>
-    </main>
+        <!-- CONTAINER -->
+        <div class="container">
 
+            <!-- HEADER -->
+            <header>
+                <img src="./public/images/logo.png" alt="logo" id="logo">
+            </header>
+
+            <!-- VP -->
+            <h1>Une url longue ? Raccourcissez-là ?</h1>
+            <h2>Largement meilleur et plus court que les autres.</h2>
+
+            <!-- FORM -->
+            <form method="post" action="../">
+                <input type="url" name="url" placeholder="Collez un lien à raccourcir">
+                <input type="submit" value="Raccourcir">
+            </form>
+
+            <?php if (isset($_GET['error']) && isset($_GET['message'])) { ?>
+                <div class="center">
+                    <div id="result">
+                        <b><?php echo htmlspecialchars($_GET['message']); ?></b>
+                    </div>
+                </div>
+            <?php } else if (isset($_GET['short'])) { ?>
+                <div class="center">
+                    <div id="result">
+                        <b>URL RACCOURCIE : </b>
+                        http://localhost/?q=<?php echo htmlspecialchars($_GET['short']); ?>
+                    </div>
+                </div>
+            <?php } ?>
+
+        </div>
+
+    </section>
+
+    <!-- BRANDS -->
+    <section id="brands">
+
+        <!-- CONTAINER -->
+        <div class="container">
+            <h3>Ces marques nous font confiance</h3>
+            <img src="./public/images/1.png" alt="1" class="picture">
+            <img src="./public/images/2.png" alt="2" class="picture">
+            <img src="./public/images/3.png" alt="3" class="picture">
+            <img src="./public/images/4.png" alt="4" class="picture">
+        </div>
+
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+        <img src="./public/images/logo2.png" alt="logo" id="logo"><br>
+        2018 © Bitly<br>
+        <a href="#">Contact</a> - <a href="#">À propos</a>
+    </footer>
 </body>
 
 </html>
